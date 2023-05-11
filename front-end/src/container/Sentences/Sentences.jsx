3fea4d83ecd85fdff5825b0b18a4fc5927a2c3ea
Divya <divyareddy0205@gmail.com>
2023-05-10 18:51:22 -0700

    folder structure

---
+++ front-end/src/container/Sentences/Sentences.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Sentences.scss";
+
+const Sentences = () => {
+  return (
+    <div>Sentences</div>
+  )
+}
+
+export default Sentences
---
+++ front-end/src/container/Vocab/Vocab.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Vocab.scss";
+
+const Vocab = () => {
+  return (
+    <div>Vocab</div>
+  )
+}
+
+export default Vocab
