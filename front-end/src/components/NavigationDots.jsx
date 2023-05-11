f4f8f66d73285a04958b89395b1034fb96662880
Divya <divyareddy0205@gmail.com>
2023-05-10 19:01:04 -0700

    folder structure

---
+++ front-end/src/components/NavigationDots.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavigationDots = () => {
+  return (
+    <div>NavigationDots</div>
+  )
+}
+
+export default NavigationDots
--- front-end/src/container/index.jsx
+++ front-end/src/container/index.jsx
@@ -1,10 +1,12 @@
-//To avoid importing individual components wherever required, importing all the files from components folder in this file.
+/*
+    To avoid importing individual components wherever required, 
+    importing all the files from container folder in this file.
+*/
 import Vocab from './Vocab/Vocab';
 import Idioms from './Idioms/Idioms';
 import Sentences from './Sentences/Sentences';
 import Grammar from './Grammar/Grammar';
 import Footer from './Footer/Footer';
-
 
 export{
     Vocab,
