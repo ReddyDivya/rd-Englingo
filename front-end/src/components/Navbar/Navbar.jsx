e19b47e7e70d4f62d5cf1d2fcb0b0ff89a344f79
Divya <divyareddy0205@gmail.com>
2023-05-11 16:41:25 -0700

    'navbar'

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -10,49 +10,47 @@ const [toggle, setToggle] = useState(false);
   return (
 
     <nav className="app__navbar">
-      {/*logo*/}
-      <div className='app__navbar-logo'>
-        <img src={images.logo} alt='logo'/>
-      </div>
+      {/* <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div> */}
+      <ul className="app__navbar-links">
 
-      {/* menu */}
-      <ul className='app__navbar-links'>
-        {
-          ['Vocab', 'Idioms', 'Sentences', 'Grammar'].map((item) => (
-            <li className='app__flex p-text' key={`link-${item}`}>
-              <a href={`${item}`}>{item}</a>
-            </li>
-          ))
-        }
+        {/*menu*/}
+        {['vocab', 'idioms', 'sentences', 'grammar'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
       </ul>
-      
 
       <div className="app__navbar-menu">
-          {/*displaying menu icon */}
-          <HiMenuAlt4 onClick={() => setToggle(true)} /> {/*setToggle = true - to open up menu */}
 
-          {
-            toggle && (
-            <motion.div
-              whileInView={{x:[300, 0]}}
-              transition={{duration:0.85, ease: 'easeOut'}}
-            >
-              {/*displaying X icon*/}
-              <HiX onClick={() => setToggle(false)}/>
-              <ul>
-                {
-                  ['Vocab', 'Idioms', 'Sentences', 'Grammar'].map((item) => ( 
-                    <li key={item}>
-                      <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-                    </li>
-                  ))
-                }
-              </ul>
-            </motion.div> 
-            )}
+         {/*displaying menu icon */}
+        <HiMenuAlt4 onClick={() => setToggle(true)} /> {/*setToggle = true - to open up menu */}
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+
+            {/*displaying X icon*/}
+            <HiX onClick={() => setToggle(false)} />  {/*setToggle = false - to close menu */}
+            <ul>
+              {['vocab', 'idioms', 'sentences', 'grammar'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
