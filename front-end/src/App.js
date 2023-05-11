0bec8c48dd80d4d41e4f8b8663b346836082b884
Divya <divyareddy0205@gmail.com>
2023-05-10 19:22:59 -0700

    app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {Vocab, Idioms, Sentences, Grammar, Footer} from './container';
+import {Navbar} from './components';
 import "./App.scss";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+        <Navbar/>
+        <Vocab/>
+        <Idioms/>
+        <Sentences/>
+        <Grammar/>
+        <Footer/>
+    </div>
   )
 }
 
