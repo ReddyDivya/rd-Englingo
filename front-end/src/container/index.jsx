4b93d0f3d004a3f688e3824efe7d943683b48c81
Divya <divyareddy0205@gmail.com>
2023-05-10 18:58:40 -0700

    folder structure

--- front-end/src/container/index.jsx
+++ front-end/src/container/index.jsx
@@ -1,3 +1,4 @@
+//To avoid importing individual components wherever required, importing all the files from components folder in this file.
 import Vocab from './Vocab/Vocab';
 import Idioms from './Idioms/Idioms';
 import Sentences from './Sentences/Sentences';
