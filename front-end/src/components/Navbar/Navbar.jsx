c5226d1cd6019bc96ef25e9f9045bdd696861186
Divya <divyareddy0205@gmail.com>
2023-05-11 11:38:52 -0700

    navbar

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import "./Navbar.scss";
+import {HiMenuAlt, HiX} from 'react-icons/hi';
+import {motion} from 'framer-motion';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <ul>
+        {
+          ['Vocab', 'Idioms', 'Sentences', 'Grammar'].map((item) => (
+            <li className='app__flex p-text' key={`link-${item}`}>
+              <a href={`${item}`}>{item}</a>
+            </li>
+          ))
+        }
+      </ul>
+    </nav>
   )
 }
 
