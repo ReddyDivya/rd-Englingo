5cfdbefd357b31dc999f168cb8c1595ddee8197f
Divya <divyareddy0205@gmail.com>
2023-05-10 18:54:11 -0700

    folder structure

---
+++ front-end/src/container/index.jsx
@@ -0,0 +1,14 @@
+import Vocab from './Vocab/Vocab';
+import Idioms from './Idioms/Idioms';
+import Sentences from './Sentences/Sentences';
+import Grammar from './Grammar/Grammar';
+import Footer from './Footer/Footer';
+
+
+export{
+    Vocab,
+    Idioms,
+    Sentences,
+    Grammar,
+    Footer
+};
