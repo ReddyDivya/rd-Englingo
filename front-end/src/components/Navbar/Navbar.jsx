2f505638b8cced076a117c3499dd6099e98812a3
Divya <divyareddy0205@gmail.com>
2023-05-11 16:34:20 -0700

    navbar

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -26,27 +26,30 @@ const [toggle, setToggle] = useState(false);
         }
       </ul>
       
-      
+
       <div className="app__navbar-menu">
           {/*displaying menu icon */}
-          {/*setToggle = true - to open up menu */}
-          <HiMenuAlt4 onClick={() => setToggle(true)}/>
-          <motion.div
-            whileInView={{x:[300, 0]}}
-            transition={{duration:0.85, ease: 'easeOut'}}
-          >
-            {/*displaying X icon*/}
-            <HiX onClick={() => setToggle(false)}/>
-            <ul>
-              {
-                ['Vocab', 'Idioms', 'Sentences', 'Grammar'].map((item) => ( 
-                  <li key={item}>
-                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-                  </li>
-                ))
-              }
-            </ul>
-          </motion.div>  
+          <HiMenuAlt4 onClick={() => setToggle(true)} /> {/*setToggle = true - to open up menu */}
+
+          {
+            toggle && (
+            <motion.div
+              whileInView={{x:[300, 0]}}
+              transition={{duration:0.85, ease: 'easeOut'}}
+            >
+              {/*displaying X icon*/}
+              <HiX onClick={() => setToggle(false)}/>
+              <ul>
+                {
+                  ['Vocab', 'Idioms', 'Sentences', 'Grammar'].map((item) => ( 
+                    <li key={item}>
+                      <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                    </li>
+                  ))
+                }
+              </ul>
+            </motion.div> 
+            )}
       </div>
     </nav>
   )
