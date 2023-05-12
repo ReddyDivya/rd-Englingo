5bb228190d87b4a2f31ad3506666fa46d736688c
Divya <divyareddy0205@gmail.com>
2023-05-11 18:36:44 -0700

    'navbar'

--- front-end/src/container/Footer/Footer.jsx
+++ front-end/src/container/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import {AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <p>2023 englingo All rights reserved.</p>
+      <p className='p-text'>2023 englingo All rights reserved.</p>
       <p className='icons'>
           <AiFillInstagram/>
           <AiOutlineTwitter/>
