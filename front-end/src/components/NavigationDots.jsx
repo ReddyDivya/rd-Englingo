be1fb3773474a4e6e7cd41036be99636b9a57bb4
Divya <divyareddy0205@gmail.com>
2023-05-11 19:24:16 -0700

    'navbar'

--- front-end/src/components/NavigationDots.jsx
+++ front-end/src/components/NavigationDots.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const NavigationDots = () => {
+//active - current visible section like 'vocab'
+const NavigationDots = ({active}) => {
   return (
-    <div>NavigationDots</div>
+    <div className='app__navigation'>
+      {
+        ['vocab', 'idioms', 'sentences', 'grammar'].map((item, index) => (
+          <a href={`${item}`}
+             key = {item+index}
+             className='app__navigation-dot'
+             style={active === item ? {backgroundColor : '#313BAC'} : {}}
+          >{item}</a>
+        ))
+      }
+    </div>
   )
 }
 
