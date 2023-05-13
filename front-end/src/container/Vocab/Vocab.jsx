0bd82b64f56ca2a386c047f342fa56a5f36732ba
Divya <divyareddy0205@gmail.com>
2023-05-13 16:59:09 -0700

    'vocab'

--- front-end/src/container/Vocab/Vocab.jsx
+++ front-end/src/container/Vocab/Vocab.jsx
@@ -22,6 +22,19 @@ const Vocab = () => {
       <h2 className='head-text'>Vocabulary</h2>
       <p className='p-text'>In this section you can do practice vocabulary.</p>
       <p className='p-text'>Read as much as possible. If you come across a word you don't know, add it down or look it up.</p>
+      {/*adding vocabs*/}
+      <div className='app__vocab-form app__flex'>
+          <div className='app__flex'>
+            <input className="p-text" type="text" placeholder="Please, enter a word" name="word" value={word} onChange={handleChangeInput} />
+          </div>
+          <div className='app__flex'>
+            <input className="p-text" type="text" placeholder="Please, enter a meaning" name="meaning" value={meaning} onChange={handleChangeInput} />
+          </div>
+          <div className='app__flex'>
+            <input className="p-text" type="text" placeholder="Please, enter a sentence" name="sentence" value={sentence} onChange={handleChangeInput} />
+          </div>
+      </div>
+
       {/*displaying vocabs items*/}
       <div className='app__vocab-items'>
           {/* vocab item card */}
