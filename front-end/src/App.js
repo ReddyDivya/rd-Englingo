6bfffef4881a5fa7a966bb2b3666b6c1cdc8a36d
Divya <divyareddy0205@gmail.com>
2023-05-15 17:43:19 -0700

    'advance-phrases'

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Vocab, Idioms, Sentences, Grammar, Footer, Very, Synonyms} from './container';
+import {Vocab, Idioms, Sentences, Grammar, Footer, Very, Synonyms, Advance} from './container';
 import {Navbar} from './components';
 import "./App.scss";
 
@@ -10,6 +10,7 @@ const App = () => {
         <Vocab/>
         <Very/>
         <Synonyms/>
+        <Advance/>
         <Idioms/>
         <Sentences/>
         <Grammar/>
