8804deadaaade3e6439a936a42d5d77e4d8a78f0
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2023-07-23 04:36:58 -0700

    Update client.js

--- front-end/src/client.js
+++ front-end/src/client.js
@@ -3,12 +3,12 @@ import {createClient}  from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: "12bn7nhe",
-  dataset: 'production',
-  apiVersion: 'v2021-10-21',
+  projectId: "",
+  dataset: '',
+  apiVersion: '',
   useCdn: true,
   //token is important for sending info from app to sanity document's fields in sanity
-  token:'skBZ8NYhg85h1UnOiH2XBIjGFmusDv2ZamaN0Tc5OyScNNlCJMWJLmDLd9sp9GoTVZPDsUUjwgAV4EVQqzntPEOtUaV9RPQ1krskC9hsF5YvTMk8LQZVAsRV2WVt4VjLTzcljqRYcwE1VCKUAjgRok1Awt3vQwlgzxCT8ajhKkLRpsMeB1jk'
+  token:''
 });
 
 const builder = imageUrlBuilder(client);
